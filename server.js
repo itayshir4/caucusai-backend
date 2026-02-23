5ea2b2663a0e5ad61cb4683ddf38ad711d03d2a1
gamer 45575 <itayshir4@gmail.com>
2026-02-23 17:20:27 +0200

    Fixed port for Render

--- server.js
+++ server.js
@@ -47,4 +47,5 @@ Generate:
   }
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
