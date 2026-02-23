c9068af4f775e3b758900d471a739039a261be9b
gamer 45575 <itayshir4@gmail.com>
2026-02-23 18:19:45 +0200

    Remove node-fetch

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ Generate:
 
   try {
 const response = await fetch(
-  `https://api.generativeai.google/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.API_KEY}`,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.API_KEY}`,
   {
     method: "POST",
     headers: { "Content-Type": "application/json" },
