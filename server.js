d7e6ff09c74dd38c567bf1c497be1276e36fbcff
gamer 45575 <itayshir4@gmail.com>
2026-02-23 17:47:55 +0200

    Switched to v1beta endpoint

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ Generate:
 `;
 
   try {
-    const response = await fetch(
-  `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.API_KEY}`,
+ const response = await fetch(
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.API_KEY}`,
   {
     method: "POST",
     headers: { "Content-Type": "application/json" },
